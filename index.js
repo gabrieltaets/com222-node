1b89b111bc0e1804ffac4c6fb168bca7e4d1946f
Gabriel Taets <gabrieltaets@gmail.com>
2018-11-23 00:18:13 -0200

    Simulating a database test

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-var lista = ['Gabriel', 'Jonathan', 'Fábio', 'Gustavo'];
-
-function existe(nome) {
-    return lista.includes(nome);
+var User = function (nome, matricula) {
+    this.nome = nome;
+    this.matricula = matricula;
 }
 
-function promiseExiste(nome) {
-    return new Promise((yes, no) => {
-        setTimeout(() => {
-            if(existe(nome)) yes(true);
-            else no();
-        }, 3000);
-    });
+var Database = function() {
+    this.data = [
+        new User('Gabriel', '2017000959'),
+        new User('Fabio', '2016006260'),
+        new User('Jonathan', '2017000850')
+    ];
+    this.getUser = (nome) => {
+        return new Promise((success, error) => {
+            setTimeout(() => {
+                let user = this.data.find(x => x.nome === nome);
+                if(user === undefined) error('Not found');
+                else success(user);
+            }, 100);
+        });
+    };
 }
+
+var db = new Database();
 
 module.exports = {
-    existe, promiseExiste
+    database: db
 }
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,34 +1,14 @@
 var assert = require('assert');
-var {existe, promiseExiste} = require('../index.js');
+var {database} = require('../index.js');
 
 describe('index.js', function() {
-
-    describe('lista', function() {
-        it('deve conter \'Gabriel\'', function() {
-            assert.equal(existe('Gabriel'), true);
-        });
-
-        it('não deve conter \'Pabblo Vittar\'', function() {
-            assert.equal(existe('Pabblo Vittar'), false);
-        });
-    });
-
-    describe('lista - promise', function() {
-        it('deve conter \'Gabriel\' após 3s', function() {
-            this.timeout(4000);
-            return promiseExiste('Gabriel').then(e => {
-                return assert.ok(e);
+    describe('Database', function () {
+        it('deve encontrar Gabriel',async function() {
+            let user = await database.getUser('Gabriel');
+            assert.deepEqual(user, {
+                nome: 'Gabriel',
+                matricula: '2017000959'
             });
-        });
-
-        it('não deve ter \'Pabblo Vittar\' após 3s', function() {
-            this.timeout(4000);
-            assert.rejects(promiseExiste('Pabblo Vittar'));
-        });
-
-        it('deve conter Jonathan', async function() {
-            this.timeout(3010);
-            assert.ok(await promiseExiste('Jonathan'));
         });
     });
 });
