6bbc1553d594919d73543d8a16673c926f48eaab
Gabriel Taets <gabrieltaets@gmail.com>
2018-11-23 01:30:32 -0200

    Reduced tab size

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,13 +2,28 @@ var assert = require('assert');
 var {database} = require('../index.js');
 
 describe('index.js', function() {
-    describe('Database', function () {
-        it('deve encontrar Gabriel',async function() {
-            let user = await database.getUser('Gabriel');
-            assert.deepEqual(user, {
-                nome: 'Gabriel',
-                matricula: '2017000959'
-            });
-        });
+  describe('Database', function () {
+    it('deve encontrar Gabriel', async function() {
+      let user = await database.getUser('Gabriel');
+      assert.deepEqual(user, {
+        nome: 'Gabriel',
+        matricula: '2017000959'
+      });
     });
+    it('deve encontar Gabriel 2', function() {
+      return database.getUser('Gabriel').then(u => {
+        assert.deepEqual(u, {
+          nome: 'Gabriel',
+          matricula: '2017000959'
+        });
+      });
+    });
+    it('nao deve encontrar José', function() {
+      return assert.rejects(database.getUser('José'));
+    });
+  });
 });
+
+/*
+Promise/then <-> async/await
+*/
